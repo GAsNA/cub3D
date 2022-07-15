f1a85c02d2668bc01447aeac29e1ae1a397d9f8f
nmathieu <nmathieu@student.42.fr>
2022-07-15 23:54:52 +0200

    normalize project

--- srcs/game/c3d_game_destroy_hook.c
+++ srcs/game/c3d_game_destroy_hook.c
@@ -6,14 +6,14 @@
 /*   By: nmathieu <nmathieu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 22:46:26 by nmathieu          #+#    #+#             */
-/*   Updated: 2022/07/15 22:51:34 by nmathieu         ###   ########.fr       */
+/*   Updated: 2022/07/15 23:53:37 by nmathieu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "c3d_game.h"
 #include "mlx.h"
 
-int		c3d_game_destroy_hook(t_game *game)
+int	c3d_game_destroy_hook(t_game *game)
 {
 	return (mlx_loop_end(game->mlx));
 }
--- srcs/game/c3d_game_load.c
+++ srcs/game/c3d_game_load.c
@@ -6,7 +6,7 @@
 /*   By: nmathieu <nmathieu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 05:22:34 by nmathieu          #+#    #+#             */
-/*   Updated: 2022/07/15 23:27:42 by nmathieu         ###   ########.fr       */
+/*   Updated: 2022/07/15 23:53:49 by nmathieu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 static void	load_images(t_game *game, const t_map *map)
 {
 	c3d_img_load_file(game->mlx, map->north_texture, &game->north_texture);
-	ft_unwind(&game, c3d_game_free_north_texture);
+	ft_unwind(game, c3d_game_free_north_texture);
 	c3d_img_load_file(game->mlx, map->south_texture, &game->south_texture);
 	ft_unwind(game, c3d_game_free_south_texture);
 	c3d_img_load_file(game->mlx, map->west_texture, &game->west_texture);
@@ -42,14 +42,15 @@ void	c3d_game_load(t_game *game, const t_map *map)
 	game->mlx = mlx_init();
 	if (!game->mlx)
 		ft_unwind_panic("failed to initialize MiniLibX");
+	unwind_index = ft_unwind(game, c3d_game_destroy_mlx);
 	game->win = mlx_new_window(
-		game->mlx,
-		C3D_WIDTH * C3D_PIXEL_SIZE,
-		C3D_HEIGHT * C3D_PIXEL_SIZE,
-		"Cub3D");
+			game->mlx,
+			C3D_WIDTH * C3D_PIXEL_SIZE,
+			C3D_HEIGHT * C3D_PIXEL_SIZE,
+			"Cub3D");
 	if (!game->mlx)
 		ft_unwind_panic("failed to create a window");
-	unwind_index = ft_unwind(game, c3d_game_destroy_window);
+	ft_unwind(game, c3d_game_destroy_window);
 	load_images(game, map);
 	game->ceiling_color = map->ceiling_color;
 	game->floor_color = map->floor_color;
